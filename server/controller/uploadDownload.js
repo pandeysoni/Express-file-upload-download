96d6f606e27fdd5632ac54a7fbc7d87964d8c13f
gaurav14 <gaurav@cronj.com>
2015-02-23 11:12:01 +0530

    add callback in create folders

--- server/controller/uploadDownload.js
+++ server/controller/uploadDownload.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
 /*
  * Display upload form
  */
+
 exports.display_form = function(req, res) {
     res.writeHead(200, {
         "Content-Type": "text/html"
@@ -15,7 +16,7 @@ exports.display_form = function(req, res) {
         '</form>'
     );
     res.end();
-}
+};
 
 /*
  * upload file
@@ -23,90 +24,84 @@ exports.display_form = function(req, res) {
 
 exports.uploadFile = function(req, res) {
     var tmp_path = req.files.file.path;
-    checkFileExist();
-    var target_path = Config.MixInsideFolder + req.files.file.name;
-    var is = fs.createReadStream(tmp_path);
-    var os = fs.createWriteStream(target_path);
-    is.pipe(os);
-    is.on('end', function() {
-        fs.unlinkSync(tmp_path);
+    checkFileExist(function(){
+        var target_path = Config.MixInsideFolder + req.files.file.name;
+        var is = fs.createReadStream(tmp_path);
+        var os = fs.createWriteStream(target_path);
+        is.pipe(os);
+        is.on('end', function() {
+            fs.unlinkSync(tmp_path);
+        });
+        res.send('File uploaded to: ' + target_path);
     });
-    res.send('File uploaded to: ' + target_path);
-}
+    
+};
 
 /*
  * Check File existence and create if not exist
  */
 
-var checkFileExist = function() {
+var checkFileExist = function(callback) {
     fs.exists(Config.publicFolder, function(exists) {
         if (exists === false) fs.mkdirSync(Config.publicFolder);
 
         fs.exists(Config.MixFolder, function(exists) {
             if (exists === false) fs.mkdirSync(Config.MixFolder);
+            callback();
         });
     });
-}
+};
 
 /**
  *get file
  */
+
 exports.getFile = function(req, res) {
     var file = req.params.file,
         path = Config.MixInsideFolder + file,
         ext = file.substr(file.lastIndexOf('.') + 1);
     fs.readFile(path, function(error, content) {
-        if (error) return reply("file not found");
+        if (error) return res.end("file not found");
+        var contentType;
         switch (ext) {
             case "pdf":
-                res.setHeader('Content-Type', 'application/pdf');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/pdf';
                 break;
             case "ppt":
-                res.setHeader('Content-Type', 'application/vnd.ms-powerpoint');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/vnd.ms-powerpoint';
                 break;
             case "pptx":
-                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.presentationml.presentation');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/vnd.openxmlformats-officedocument.presentationml.presentation';
                 break;
             case "xls":
-                res.setHeader('Content-Type', 'application/vnd.ms-excel');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/vnd.ms-excel';
                 break;
             case "xlsx":
-                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
                 break;
             case "doc":
-                res.setHeader('Content-Type', 'application/msword');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/msword';
                 break;
             case "docx":
-                res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
                 break;
             case "csv":
-                res.setHeader('Content-Type', 'application/octet-stream');
-                res.setHeader("Content-Disposition", "attachment; filename=" + file);
-                res.end(content, 'binary');
+                contentType = 'application/octet-stream';
                 break;
             default:
                 res.download(path);
         }
+
+        res.setHeader('Content-Type', contentType);
+        res.setHeader("Content-Disposition", "attachment; filename=" + file);
+        res.end(content, 'binary');
     });
 };
 
 /**
  *get fileList
  */
+
 exports.fileList = function(req, res) {
     var files = [];
     // Walker options
@@ -123,4 +118,4 @@ exports.fileList = function(req, res) {
     walker.on('end', function() {
         return res.json(files);
     });
-}
+};
